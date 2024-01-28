d009388f0969d59baa9ca685884cd7cd66ac2d2c
Kunle <OS409@live.mdx.ac.uk>
2024-01-28 22:49:21 +0000

    asddas

--- server.js
+++ server.js
@@ -56,15 +56,17 @@ app.post('/collections/:collectionName', function(req, res, next) {
     });
 });
 
-app.put('/collections/:collectionName/:orderId', async function(req, res, next) {
-    const orderId = req.params.orderId;
+app.put('/collections/:collectionName', async function(req, res, next) {
+    const ids = req.body;
 
     try {
-        await req.collection.updateOne(
-            { id: orderId },
-            { $inc: { spaces: -1 } },
-            { safe: true }
-        );
+        for (const id of ids) {
+            await req.collection.updateOne(
+                { id: id },
+                { $inc: { spaces: -1 } },
+                { safe: true }
+            );
+        }
 
         res.send({ msg: 'success' });
     } catch (err) {
